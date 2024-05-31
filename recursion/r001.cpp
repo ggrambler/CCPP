fa15d0692a47c8518be8ffeba620b4e61dc71252
ggrambler <jha02032003@gmail.com>
2024-05-31 19:44:21 +0530

    fact

---
+++ recursion/r001.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int fact(int n){
+    if(n)
+        return (n*fact(n-1));
+    else
+        return 1;
+}
+
+
+int main(){
+    int a = fact(5);
+    printf("%d",a);
+
+
+    return 0;
+}
